ac4ab2f97eb89825129ca89fcd0a4d17767d541e
David Mullins <me@davit.ie>
2025-07-09 11:04:34 +0200

    feat(main.sh): add test simple to tes1 and test_usb_info to test2

--- scripts/main.sh
+++ scripts/main.sh
@@ -17,40 +17,42 @@ fi
 
 # Commands
 case "$1" in
-  lint)
-    echo "Running lint..."
-    shellcheck "$SCRIPT_DIR/usb_info.sh"
-    prettier --check "$SCRIPT_DIR/../."
-    ;;
-  format)
-    echo "Formatting code..."
-    prettier --write "$SCRIPT_DIR/../."
-    ;;
-  test)
-    echo "Running tests..."
-    sudo bash "$SCRIPT_DIR/../tests/test_usb_info.sh"
-    ;;
-  run)
-    echo "Running usb_info.sh..."
-    sudo bash "$SCRIPT_DIR/usb_info.sh"
-    cat "$LOG_DIR/usb_script.log"
-    ;;
-  commit)
-    echo "Committing changes..."
-    git add .
-    npx cz
-    ;;
-  release)
-    echo "Creating release..."
-    git tag "v$(jq -r '.version' package.json)"
-    git push origin main
-    git push origin --tags
-    ;;
-  *)
-    echo "Usage: $0 {lint|format|test|run|commit|release}"
-    exit 1
-    ;;
+lint)
+  echo "Running lint..."
+  shellcheck "$SCRIPT_DIR/usb_info.sh"
+  prettier --check "$SCRIPT_DIR/."
+  ;;
+format)
+  echo "Formatting code..."
+  prettier --write "$SCRIPT_DIR/."
+  ;;
+test1)
+  sudo bash "$BASE_DIR/tests/test_usb_simple.sh"
+  ;;
+test2)
+  echo "Running tests..."
+  sudo bash "$BASE_DIR/tests/test_usb_info.sh"
+  ;;
+run)
+  echo "Running usb_info.sh..."
+  sudo bash "$SCRIPT_DIR/usb_info.sh"
+  cat "$LOG_DIR/usb_script.log"
+  ;;
+commit)
+  echo "Committing changes..."
+  git add .
+  npx cz
+  ;;
+release)
+  echo "Creating release..."
+  git tag "v$(jq -r '.version' package.json)"
+  git push origin main
+  git push origin --tags
+  ;;
+*)
+  echo "Usage: $0 {lint|format|test1|test2|run|commit|release}"
+  exit 1
+  ;;
 esac
-
 
 # end of main.sh
